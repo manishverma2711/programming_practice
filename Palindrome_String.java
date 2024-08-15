097c5e1c296149b29899384380bc6d3cb2587db7
Manish <manish2711.mv@gmail.com>
2024-08-15 14:55:09 +0530

    Find the string is palindrome or not

--- Palindrome_String.java
+++ Palindrome_String.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Palindrome_String {
+import java.util.Scanner;
+
+public class Palindrome_String {
+    public static void main(String[] args) {
+
+        System.out.print(" Enter a string :");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String rev ="";
+
+        for ( int i= str.length()-1;i>=0;i--){
+
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println(rev);
+
+        if (str.equals(rev)){
+
+            System.out.println(" Its palindrome string :");
+        }
+        else
+        {
+            System.out.println("Its non- palindrome string :");
+        }
+
+    }
 }
