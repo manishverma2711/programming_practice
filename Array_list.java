01ce69a69049ed5289d0b998b75cffe6b0fbd8b1
Manish <manish2711.mv@gmail.com>
2024-08-16 14:09:57 +0530

    Find the element from the arraylist

--- Array_list.java
+++ Array_list.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Array_list {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array_list {
+
+    public static void main(String[] args) {
+
+        List<String> a = new ArrayList<String>();
+
+      a.add("Manish");
+      a.add("Sandeep");
+      a.add("Champ");
+
+        System.out.println("The second array is: " + a.get(1));
+
+        System.out.println(a.size());
+
+
+    }
 }
